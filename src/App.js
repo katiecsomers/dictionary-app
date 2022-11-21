2db9da1d8813fc1b201423a4f762ffbb7dfc2bb0
katiecsomers <kcornallysomers@gmail.com>
2022-11-21 15:11:05 +1300

    File structure, initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">Dictionary App</header>
+		</div>
+	);
+}
+
+export default App;
